fdf3f828dabec4685ebfa714e510994397fd4cbe
Anthony Corbin <dragon1672@gmail.com>
2017-07-15 00:16:09 -0700

    adding stubs for game ideas

---
+++ src/main/java/lights_out/Driver.java
@@ -0,0 +1,11 @@
+package lights_out;
+
+import common.utils.Flogger;
+
+public class Driver {
+    private static final Flogger logger = Flogger.getInstance();
+
+    public static void main(String... args) {
+        logger.atInfo().log("Hello World");
+    }
+}
