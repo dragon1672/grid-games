8fa61ce473c545e8826dfdad53145987eb53a00c
Anthony Corbin <dragon1672@gmail.com>
2017-07-15 00:19:37 -0700

    Adding tests for IntVector

---
+++ src/test/java/common/utils/IntVector2Test.java
@@ -0,0 +1,34 @@
+package common.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntVector2Test {
+    @Test
+    public void testCreation() {
+        IntVector2 vector = IntVector2.of(1, 2);
+        Assert.assertEquals(1, vector.x);
+        Assert.assertEquals(2, vector.y);
+    }
+
+    @Test
+    public void testEquals() {
+        Assert.assertEquals(IntVector2.of(12, 34), IntVector2.of(12, 34));
+    }
+
+    @Test
+    public void testAdd() {
+        IntVector2 a = IntVector2.of(1, 2);
+        IntVector2 b = IntVector2.of(4, 5);
+
+        Assert.assertEquals(IntVector2.of(5, 7), a.add(b));
+    }
+
+    @Test
+    public void testThatAddIsImmutable() {
+        IntVector2 original = IntVector2.of(12, 34);
+        original.add(IntVector2.of(56, 78));
+
+        Assert.assertEquals(IntVector2.of(12, 34), original);
+    }
+}
