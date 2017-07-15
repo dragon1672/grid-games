417156a791d0e22a55d23f3a181e00d50356b907
Anthony Corbin <dragon1672@gmail.com>
2017-07-15 13:55:50 -0700

    Adding GUI for floodit game

--- src/main/java/flood_it/Driver.java
+++ src/main/java/flood_it/Driver.java
@@ -1,6 +1,7 @@
 package flood_it;
 
 import com.google.common.collect.ImmutableList;
+import common.gui.BoardGui;
 import common.utils.AsciiBoard;
 import common.utils.Flogger;
 import flood_it.ai.AI;
@@ -10,6 +11,7 @@ import flood_it.ai.RandomAi;
 import flood_it.game.BlockColor;
 import flood_it.game.FloodIt;
 
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -22,7 +24,26 @@ public class Driver {
     @SuppressWarnings("unused")
     private static final List<AI> AIs = ImmutableList.of(new RandomAi(), new MaxPerimeterAi(), new MaxAreaAi());
 
-    private static void runAi(AI ai, FloodIt game) {
+    private static Color cell2Color(BlockColor blockColor) {
+        switch (blockColor) {
+            case RED:
+                return Color.RED;
+            case YELLOW:
+                return Color.YELLOW;
+            case GREEN:
+                return Color.GREEN;
+            case LIGHT_BLUE:
+                return Color.CYAN;
+            case BLUE:
+                return Color.BLUE;
+            case PURPLE:
+                return Color.MAGENTA;
+        }
+        throw new IllegalArgumentException("wat");
+    }
+
+    private static void runAi(AI ai, FloodIt game) throws InterruptedException {
+        BoardGui<BlockColor> gui = BoardGui.createColorBoard(Driver::cell2Color);
         logger.atInfo().log("Staring AI moves");
 
         int moves = 0;
@@ -32,13 +53,15 @@ public class Driver {
             BlockColor move = ai.getMove(game);
             logger.atInfo().log("AI picked color %s", move);
             game.floodColor(move);
+            gui.updateBoard(game.getBoard());
             logger.atInfo().log("board state\n%s", AsciiBoard.boardToString(game.getBoard()));
+            Thread.sleep(1000);
         }
 
         logger.atInfo().log("Game Complete after %d rounds", moves);
     }
 
-    public static void main(String... args) {
+    public static void main(String... args) throws InterruptedException {
 
         logger.atInfo().log("Staring game");
 
