5395988ad7c4dae9301485b54abd30d724ba88fb
Anthony Corbin <dragon1672@gmail.com>
2017-08-06 00:46:14 -0700

    Changing AI to use stream to allow for larger boards (with terrible performance, but allowed)

--- src/main/java/lights_out/ai/BillyTheBruteForceLightsOutAI.java
+++ src/main/java/lights_out/ai/BillyTheBruteForceLightsOutAI.java
@@ -1,16 +1,18 @@
 package lights_out.ai;
 
-import com.google.common.collect.Sets;
 import common.board.Board;
 import common.board.BoardImpl;
 import common.board.ReadOnlyBoard;
 import common.gui.BoardGui;
 import common.utils.BoardUtils;
 import common.utils.IntVector2;
+import common.utils.SetOperations;
 import lights_out.game.LightsOutBoardUtils;
 import lights_out.game.LightsOutCell;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 
 public class BillyTheBruteForceLightsOutAI implements LightsOutAI {
     private final BoardGui<LightsOutCell> gui;
@@ -27,6 +29,7 @@ public class BillyTheBruteForceLightsOutAI implements LightsOutAI {
 
     @Override
     public Set<IntVector2> getMoves(ReadOnlyBoard<LightsOutCell> board) {
+        /*
         // power set doesn't for for a set of over 30 elements, I'll need to make my own that can handle larger boards
         Set<Set<IntVector2>> allPossibleMoves = Sets.powerSet(BoardUtils.boardPositions(board));
         for (Set<IntVector2> possibleMoves : allPossibleMoves) {
@@ -37,5 +40,25 @@ public class BillyTheBruteForceLightsOutAI implements LightsOutAI {
             }
         }
         throw new IllegalArgumentException("board unsolvable");
+        /*/
+
+        // This appears to be causing a "Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException" but I'm not sure why
+        Optional<Set<IntVector2>> winningMoves = SetOperations.powerSet(BoardUtils.boardPositions(board))
+                // process result and convert to an optional for if we find a winning move
+                .map((Function<Set<IntVector2>, Optional<Set<IntVector2>>>) possibleMove -> {
+                    ReadOnlyBoard<LightsOutCell> resultingBoard = getBoardWithMoves(board, possibleMove);
+                    gui.updateBoard(resultingBoard);
+                    if (BoardUtils.isUniformColor(resultingBoard)) {
+                        return Optional.of(possibleMove);
+                    }
+                    return Optional.empty();
+                })
+                // Flattening Optional
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+
+        return winningMoves.orElseThrow(() -> new IllegalArgumentException("board unsolvable"));
+        //*/
     }
 }
