1cc525b9db5b171d8c7234a7a059a0b907be31b7
Anthony Corbin <dragon1672@gmail.com>
2017-08-19 17:14:42 -0700

    Removing TODO for 2048 game

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
         System.out.println("This is a collection of simple grid based games");
         System.out.println("Here are some additional game ideas");
         System.out.println("Tents: https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/tents.html");
-        System.out.println("2048");
     }
 }
