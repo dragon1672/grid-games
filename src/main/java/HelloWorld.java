40b05177d6b0e98c0c90e3703605e70d85be2364
Anthony Corbin <dragon1672@gmail.com>
2017-08-19 17:23:45 -0700

    Removing useless class that used to just hold comments

--- src/main/java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String... args) {
-        System.out.println("This is a collection of simple grid based games");
-        System.out.println("Here are some additional game ideas");
-        System.out.println("Tents: https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/tents.html");
-    }
-}
