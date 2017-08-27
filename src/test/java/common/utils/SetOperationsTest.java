0124745157af155e0a41ae80e222e4e782e9a5dc
Anthony Corbin <dragon1672@gmail.com>
2017-08-26 22:33:14 -0700

    Refactoring set operations tests to use test factories to make output easier to read

--- src/test/java/common/utils/SetOperationsTest.java
+++ src/test/java/common/utils/SetOperationsTest.java
@@ -1,10 +1,14 @@
 package common.utils;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -12,6 +16,7 @@ import java.util.stream.Stream;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class SetOperationsTest {
     private static <T> void assertEqualPowerSets(Set<Set<T>> output, Set<Set<T>> expected) {
@@ -27,16 +32,25 @@ class SetOperationsTest {
         assertThat(toCheck).named("extra elements").isEmpty();
     }
 
-    private static <T> void testPowerSetStream(Function<Set<T>, Stream<Set<T>>> function, Set<T> input) {
-        Set<Set<T>> expectedOutput = Sets.powerSet(input);
-        Set<Set<T>> output = function.apply(input).collect(Collectors.toSet());
-        assertEqualPowerSets(output, expectedOutput);
+    private static <T> DynamicTest powerSetStreamTest(Function<Set<T>, Stream<Set<T>>> function, Set<T> input) {
+        return dynamicTest(
+                input.size() + " elements",
+                () -> {
+                    Set<Set<T>> expectedOutput = Sets.powerSet(input);
+                    Set<Set<T>> output = function.apply(input).collect(Collectors.toSet());
+                    assertEqualPowerSets(output, expectedOutput);
+                });
     }
 
-    private static <T> void testPowerSet(Function<Set<T>, Set<Set<T>>> function, Set<T> input) {
-        Set<Set<T>> expectedOutput = Sets.powerSet(input);
-        Set<Set<T>> output = function.apply(input);
-        assertEqualPowerSets(output, expectedOutput);
+    private static <T> DynamicTest powerSetTest(Function<Set<T>, Set<Set<T>>> function, Set<T> input) {
+        return dynamicTest(
+                input.size() + " elements",
+                () -> {
+                    Set<Set<T>> expectedOutput = Sets.powerSet(input);
+                    Set<Set<T>> output = function.apply(input);
+                    assertEqualPowerSets(output, expectedOutput);
+                }
+        );
     }
 
     @Test
@@ -81,149 +95,81 @@ class SetOperationsTest {
     @Test
     void testMyPowerEquals_ExtraElement() throws Exception {
         assertThrows(AssertionError.class, () -> assertEqualPowerSets(
-                    ImmutableSet.of(
-                            ImmutableSet.of(1),
-                            ImmutableSet.of(1, 2),
-                            ImmutableSet.of(1, 2, 3)
-                    ),
-                    ImmutableSet.of(
-                            ImmutableSet.of(1),
-                            ImmutableSet.of(1, 2)
-                    )
+                ImmutableSet.of(
+                        ImmutableSet.of(1),
+                        ImmutableSet.of(1, 2),
+                        ImmutableSet.of(1, 2, 3)
+                ),
+                ImmutableSet.of(
+                        ImmutableSet.of(1),
+                        ImmutableSet.of(1, 2)
+                )
         ));
     }
 
-    @Test
-    void powerSet_Recursive_0() throws Exception {
-        testPowerSet(SetOperations::powerSet_Recursive, ImmutableSet.of());
+    @TestFactory
+    List<DynamicTest> powerSet_Recursive() {
+        return ImmutableList.of(
+                powerSetTest(SetOperations::powerSet_Recursive, ImmutableSet.of()),
+                powerSetTest(SetOperations::powerSet_Recursive, ImmutableSet.of(1)),
+                powerSetTest(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2)),
+                powerSetTest(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2, 3)),
+                powerSetTest(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2, 3, 4))
+        );
     }
 
-    @Test
-    void powerSet_Recursive_1() throws Exception {
-        testPowerSet(SetOperations::powerSet_Recursive, ImmutableSet.of(1));
+    @TestFactory
+    List<DynamicTest> powerSet_iterative() {
+        return ImmutableList.of(
+                powerSetTest(SetOperations::powerSet_iterative, ImmutableSet.of()),
+                powerSetTest(SetOperations::powerSet_iterative, ImmutableSet.of(1)),
+                powerSetTest(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2)),
+                powerSetTest(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2, 3)),
+                powerSetTest(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2, 3, 4))
+        );
     }
 
-    @Test
-    void powerSet_Recursive_2() throws Exception {
-        testPowerSet(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2));
+    @TestFactory
+    List<DynamicTest> powerSet_bitSet() {
+        return ImmutableList.of(
+                powerSetTest(SetOperations::powerSet_bitSet, ImmutableSet.of()),
+                powerSetTest(SetOperations::powerSet_bitSet, ImmutableSet.of(1)),
+                powerSetTest(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2)),
+                powerSetTest(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2, 3)),
+                powerSetTest(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2, 3, 4))
+        );
     }
 
-    @Test
-    void powerSet_Recursive_3() throws Exception {
-        testPowerSet(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2, 3));
+    @TestFactory
+    List<DynamicTest> stream_powerSet_bitSet() {
+        return ImmutableList.of(
+                powerSetStreamTest(SetOperations::stream_powerSet_bitSet, ImmutableSet.of()),
+                powerSetStreamTest(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1)),
+                powerSetStreamTest(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2)),
+                powerSetStreamTest(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2, 3)),
+                powerSetStreamTest(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2, 3, 4))
+        );
     }
 
-    @Test
-    void powerSet_Recursive_4() throws Exception {
-        testPowerSet(SetOperations::powerSet_Recursive, ImmutableSet.of(1, 2, 3, 4));
+    @TestFactory
+    List<DynamicTest> stream_powerSet() {
+        return ImmutableList.of(
+                powerSetStreamTest(SetOperations::powerSet, ImmutableSet.of()),
+                powerSetStreamTest(SetOperations::powerSet, ImmutableSet.of(1)),
+                powerSetStreamTest(SetOperations::powerSet, ImmutableSet.of(1, 2)),
+                powerSetStreamTest(SetOperations::powerSet, ImmutableSet.of(1, 2, 3)),
+                powerSetStreamTest(SetOperations::powerSet, ImmutableSet.of(1, 2, 3, 4))
+        );
     }
 
-    @Test
-    void powerSet_iterative_0() throws Exception {
-        testPowerSet(SetOperations::powerSet_iterative, ImmutableSet.of());
-    }
-
-    @Test
-    void powerSet_iterative_1() throws Exception {
-        testPowerSet(SetOperations::powerSet_iterative, ImmutableSet.of(1));
-    }
-
-    @Test
-    void powerSet_iterative_2() throws Exception {
-        testPowerSet(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2));
-    }
-
-    @Test
-    void powerSet_iterative_3() throws Exception {
-        testPowerSet(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2, 3));
-    }
-
-    @Test
-    void powerSet_iterative_4() throws Exception {
-        testPowerSet(SetOperations::powerSet_iterative, ImmutableSet.of(1, 2, 3, 4));
-    }
-
-    @Test
-    void powerSet_bitSet_0() throws Exception {
-        testPowerSet(SetOperations::powerSet_bitSet, ImmutableSet.of());
-    }
-
-    @Test
-    void powerSet_bitSet_1() throws Exception {
-        testPowerSet(SetOperations::powerSet_bitSet, ImmutableSet.of(1));
-    }
-
-    @Test
-    void powerSet_bitSet_2() throws Exception {
-        testPowerSet(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2));
-    }
-
-    @Test
-    void powerSet_bitSet_3() throws Exception {
-        testPowerSet(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2, 3));
-    }
-
-    @Test
-    void powerSet_bitSet_4() throws Exception {
-        testPowerSet(SetOperations::powerSet_bitSet, ImmutableSet.of(1, 2, 3, 4));
-    }
-
-    @Test
-    void stream_powerSet_bitSet_0() throws Exception {
-        testPowerSetStream(SetOperations::stream_powerSet_bitSet, ImmutableSet.of());
-    }
-
-    @Test
-    void stream_powerSet_bitSet_1() throws Exception {
-        testPowerSetStream(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1));
-    }
-
-    @Test
-    void stream_powerSet_bitSet_2() throws Exception {
-        testPowerSetStream(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2));
-    }
-
-    @Test
-    void stream_powerSet_bitSet_3() throws Exception {
-        testPowerSetStream(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2, 3));
-    }
-
-    @Test
-    void stream_powerSet_bitSet_4() throws Exception {
-        testPowerSetStream(SetOperations::stream_powerSet_bitSet, ImmutableSet.of(1, 2, 3, 4));
-    }
-
-    @Test
-    void stream_powerSet_0() throws Exception {
-        testPowerSetStream(SetOperations::powerSet, ImmutableSet.of());
-    }
-
-    @Test
-    void stream_powerSet_1() throws Exception {
-        testPowerSetStream(SetOperations::powerSet, ImmutableSet.of(1));
-    }
-
-    @Test
-    void stream_powerSet_2() throws Exception {
-        testPowerSetStream(SetOperations::powerSet, ImmutableSet.of(1, 2));
-    }
-
-    @Test
-    void stream_powerSet_3() throws Exception {
-        testPowerSetStream(SetOperations::powerSet, ImmutableSet.of(1, 2, 3));
-    }
-
-    @Test
-    void stream_powerSet_4() throws Exception {
-        testPowerSetStream(SetOperations::powerSet, ImmutableSet.of(1, 2, 3, 4));
-    }
-
-    @Test
-    void elementIsIn() {
-        assertThat(SetOperations.elementIsIn(1, 2, 3).test(1)).isTrue();
-        assertThat(SetOperations.elementIsIn(1, 2, 3).test(2)).isTrue();
-        assertThat(SetOperations.elementIsIn(1, 2, 3).test(3)).isTrue();
-        assertThat(SetOperations.elementIsIn(1, 2, 3).test(4)).isFalse();
-        assertThat(SetOperations.elementIsIn(1, 2, 3).test(-1)).isFalse();
+    @TestFactory
+    List<DynamicTest> elementIsIn() {
+        return ImmutableList.of(
+                dynamicTest(" 1", () -> assertThat(SetOperations.elementIsIn(1, 2, 3).test(1)).isTrue()),
+                dynamicTest(" 2", () -> assertThat(SetOperations.elementIsIn(1, 2, 3).test(2)).isTrue()),
+                dynamicTest(" 3", () -> assertThat(SetOperations.elementIsIn(1, 2, 3).test(3)).isTrue()),
+                dynamicTest(" 4", () -> assertThat(SetOperations.elementIsIn(1, 2, 3).test(4)).isFalse()),
+                dynamicTest("-1", () -> assertThat(SetOperations.elementIsIn(1, 2, 3).test(-1)).isFalse())
+        );
     }
 }
